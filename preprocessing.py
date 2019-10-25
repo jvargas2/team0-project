6a91bb9e52ad4b22ea31e694bd9b185db578562e
Jorge Vargas <jvargas2@icloud.com>
2019-10-24 22:13:32 -0400

    Add evaluate functions

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import os
+from math import sqrt
 import pandas as pd
 
 def load_df(name='training.csv'):
@@ -19,6 +20,45 @@ def example_model(df):
 def evaluate(df):
     if 'prediction' not in df.columns:
         raise ValueError('Needs a prediction column to evaluate')
+
+    labels = ['DNA', 'RNA', 'DRNA', 'nonDRNA']
+    label_results = {}
+
+    for label in labels:
+        tp = df.get(df['class'] == label).get(df['prediction'] == label).count()[0].item()
+        tn = df.get(df['class'] != label).get(df['prediction'] != label).count()[0].item()
+        fp = df.get(df['class'] != label).get(df['prediction'] == label).count()[0].item()
+        fn = df.get(df['class'] == label).get(df['prediction'] != label).count()[0].item()
+
+        total = tp + tn + fp + fn
+        accuracy = 100 * (tp + tn) / total
+        sensitivity = 100 * tp / (tp + fn)
+        specificity = 100 * tn / (tn + fp)
+
+        denominator = sqrt((tp + fn) * (tp + fn) * (tn + fp) * (tn + fn))
+        if denominator == 0:
+            denominator = 1
+        mcc = (tp * fn - fp * fn) / denominator
+        
+        label_results[label] = {
+            'tp': tp,
+            'tn': tn,
+            'fp': fp,
+            'fn': fn,
+            'accuracy': accuracy,
+            'sensitivity': sensitivity,
+            'specificity': specificity,
+            'mcc': mcc
+        }
+
+    for label, results in label_results.items():
+        print('----%s----' % label)
+        for key, value in results.items():
+            print('  %s: %f' % (key, value))
+
+def evaluate_file(filename):
+    df = pd.load_csv(filename)
+    evaluate(df)
 
 if __name__ == '__main__':
     df = load_df()
