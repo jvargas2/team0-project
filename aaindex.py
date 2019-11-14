f60ba8c1be74243392c837be94c0e3dd92fadb82
Evan French <etfrench@vcu.edu>
2019-11-14 07:11:19 -0500

    Code to generate AAIndex 2D features

--- aaindex.py
+++ aaindex.py
@@ -10,7 +10,12 @@ class AaIndex(object):
 
         df = preprocessing.load_df()
         aa_index = AaIndex()
+
+        # Write features to CSV
         aa_index.write_to_csv(df)
+
+        # Write tensor for Bi-LSTM to file
+        aa_index.write_to_file(df)
     """
 
     def __init__(self):
@@ -74,8 +79,31 @@ class AaIndex(object):
             # Write protein features
             for ix, record in df.iterrows():
                 csvfile.write("\n")
-                x = [torch.tensor(self.features[aa]) for aa in record.protein if aa in self.alphabet]
+                x = [torch.tensor(aa_index.features[aa]) if aa in aa_index.alphabet else torch.zeros(feature_cnt) for aa in record.protein]
                 row = torch.stack(x, dim=0).mean(dim=0).numpy()
                 row = '\t'.join(map(str, row))
                 csvfile.write(row)   
 
+    def write_to_file(self, df):
+        """Write AAIndex features to a file. 
+        All sequences are zerro padded at the end to make them identical lengths.
+
+        aaindex_MAXL_553.pt - contains a stacked tensor for all sequences of shape [N, max(L), 553]
+        where L is the length of each protein, and 553 is the # of AAIndex features
+        """
+        # Pad sequences
+        feature_cnt = len(self.header)
+        max_seq_len = len(max(df.protein, key=len))
+        seqs = [protein + '0'*(max_seq_len-len(protein)) for protein in df.protein]
+
+        # Create tensors
+        tensor_list = []
+        for seq in seqs:
+            x = [torch.tensor(self.features[aa]) if aa in self.alphabet else torch.zeros(feature_cnt) for aa in seq]
+            protein_tensor = torch.stack(x, dim=0)
+            tensor_list.append(protein_tensor)
+
+        # Save a tensor of shape [N, max(L), 553].
+        # Where N is the # of proteins, L is the length of each protein, and 553 is the # of AAIndex features
+        stacked_tensor = torch.stack(tensor_list)
+        torch.save(stacked_tensor, 'data/features/aaindex_MAXL_553.pt')
